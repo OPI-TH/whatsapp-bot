bca2600100da2c779201c806b041f3f3bd0a27d3
OPI TH <opiththoriqulhuda@gmail.com>
2023-06-10 18:14:31 +0000

    v2

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,12 +1,13 @@
-FROM gitpod/workspace-full:latest
+FROM node:slim AS app
 
-USER root
+# We don't need the standalone Chromium
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
 
-RUN sudo apt-get update && sudo apt-get upgrade -y
-RUN sudo apt-get install ca-certificates fonts-liberation libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils -y
-RUN install-packages gnupg \
-    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/googlechrome-linux-keyring.gpg \
-    && sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/googlechrome-linux-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
-    && install-packages google-chrome-stable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-khmeros fonts-kacst fonts-freefont-ttf libxss1
-
-USER gitpod
+# Install Google Chrome Stable and fonts
+# Note: this installs the necessary libs to make the browser work with Puppeteer.
+RUN apt-get update && apt-get install curl gnupg -y \
+  && curl --location --silent https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+  && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+  && apt-get update \
+  && apt-get install google-chrome-stable -y --no-install-recommends \
+  && rm -rf /var/lib/apt/lists/*
--- index.js
+++ index.js
@@ -1,41 +1,62 @@
-const wa = require('@open-wa/wa-automate');
+const wa = require('whatsapp-web.js')
 const fs = require('fs');
 const express = require("express")
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser')
+const qrcode = require('qrcode-terminal');
 
-wa.create({
-  sessionId: "BOT_WA_OPITH",
-  multiDevice: true, //required to enable multiDevice support
-  blockCrashLogs: true,
-  disableSpins: true,
-  hostNotificationLang: 'PT_BR',
-  logConsole: false,
-  popup: true,
-  qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
-}).then(client => start(client));
+const client = new wa.Client({
+  puppeteer: {
+		args: ['--no-sandbox'],
+	},
+  authStrategy: new wa.LocalAuth({ clientId: "client-one" })
+})
 
 const prefix = 'v!'
 
-function start(client) {
-    // Initialize express and define a port
-    const app = express()
-    const PORT = 3000
-    // Tell express to use body-parser's JSON parsing
-    app.use(bodyParser.json())
-    // Start express on the defined port
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+  const app = express()
+  const PORT = 3000
+// Tell express to use body-parser's JSON parsing
+  app.use(bodyParser.json())
+// Start express on the defined port
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+  app.use(bodyParser.json())
+  app.post("/", (req, res) => {
+    const pesan = `Pendaftaran Fesban Sukses Dengan : 
+    Nama Grup : ${req.body['Nama Group']} 
+    Nama Pimpinan : ${req.body['Nama Pimpinan/Pendamping']} 
+    Nomer Telepon/Whatsapp : ${req.body['Nomor Telepon/Whatsapp']}
+    Daftar Nama Peserta : ${req.body['Nama Peserta']}
+    Dari Instansi : ${req.body['Asal Instansi']}
+    Kota/Kabupaten : ${req.body['Kota/Kabupaten']} 
+    Kecamatan : ${req.body.Kecamatan}
+    Desa : ${req.body.Desa}
+    Alamat : ${req.body.Alamat}
+    Ini Adalah Pesan Otomatis. Terimakasih`
+
+    console.log(pesan)// Call your action on the request here 
+    
+    client.sendMessage('6282142968885@c.us',pesan);
+    client.sendMessage('6285234438685@c.us',pesan);
+    res.status(200).end()
+  })
+})
   let commands = new Map()
   const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for(const file of commandFiles) {
-  const commandFile = require(`./commands/${file}`)
-  commands.set(commandFile.name, commandFile);
-}
-console.log(commands)
-  client.onMessage(async message => {
+  for(const file of commandFiles) {
+    const commandFile = require(`./commands/${file}`)
+    commands.set(commandFile.name, commandFile);
+  }
+  console.log(commands)
+  
+  client.on('message', async message => {
     if(!message.body.toLowerCase().startsWith(prefix)) return;
-    if(message.from === '6285607347186@c.us') return await client.sendText(message.from, 'Nice Bro GG, Lo Udh Di Blacklist GBLK !');
-    else if(message.author && message.author === '6285607347186@c.us') return await client.sendText(message.from, 'Nice Bro GG, Lo Udh Di Blacklist GBLK !');
     const args = message.body.slice(prefix.length).trim().split(/ +/g);
     let cmd = args.shift()
     let command = commands.get(cmd)
@@ -44,9 +65,10 @@ console.log(commands)
           command.run(client, message, args);
         } catch (error) {
           console.error(error);
-          message.reply('there was an error trying to execute that command!');
+          message.reply('Error, Mohon laporkan ke pengembang !');
         }
     })
-  }
+  // Responding is important
+  client.initialize();
 
   // Initialize express and define a port
