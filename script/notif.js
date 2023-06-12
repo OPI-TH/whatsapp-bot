3fa2c7fcceeb51fa5ab2b6e5cd713e701bbdf2c1
OPI TH <opiththoriqulhuda@gmail.com>
2023-06-12 17:59:51 +0000

    fix webhook

--- script/notif.js
+++ script/notif.js
@@ -9,8 +9,9 @@ module.exports = {
           });
           const app = express()
           const PORT = 3000
-        // Tell express to use body-parser's JSON parsing
-        // Start express on the defined port
+          app.use(bodyParser.json())
+          app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+          
           app.use(bodyParser.json())
           app.post("/", (req, res) => {
             const pesan = `Pendaftaran Fesban Sukses Dengan : 
